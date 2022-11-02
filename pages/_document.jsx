304f3095ee6f25fc2dad9c171f682bedb63f2447
louis <louis@huort.com>
2022-11-02 22:07:04 +0100

    fix: http request

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -8,6 +8,10 @@ export default function Document() {
           rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
         />
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        ></meta>
       </Head>
       <body>
         <Main />
