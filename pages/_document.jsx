e7f20300e9d99438a99e9205a7954c39e098f2fa
louis <louis@huort.com>
2022-11-02 22:44:09 +0100

    fix: remove update policy

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -8,10 +8,6 @@ export default function Document() {
           rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
         />
-        <meta
-          http-equiv="Content-Security-Policy"
-          content="upgrade-insecure-requests"
-        ></meta>
       </Head>
       <body>
         <Main />
